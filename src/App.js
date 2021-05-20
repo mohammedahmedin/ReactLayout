2520148769be4ad3fb71566c830b7ae7f9c0fdf8
mohammed ahmedin <mohammed.ahmedin@outlook.com>
2021-05-21 00:09:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = " Welcome to Senegal"
+  const like = " 50"
+  const link= "http://www.google.com"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="div content">
+        <h1>{title}</h1>
+        <p>liked it {like} times</p>
+        <p>{10}</p>
+        <p>{"hello, ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5, 6]}</p>
+        <p>{Math.random() * 10 }</p>
+
+        <a href= {link}> Link to google</a>
+      </div>
+
     </div>
   );
 }
